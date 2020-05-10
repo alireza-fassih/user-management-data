636fb85d2a345ad01dc42a8189178d3f68d4e474
alirezafassih <fassih.alireza@gmail.com>
2020-05-10 09:26:58 +0430

    add assertion to test

--- src/test/java/ir/fassih/usermanagementdata/UserManagementDataApplicationTests.java
+++ src/test/java/ir/fassih/usermanagementdata/UserManagementDataApplicationTests.java
@@ -1,13 +1,22 @@
 package ir.fassih.usermanagementdata;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+
 
 @SpringBootTest
 class UserManagementDataApplicationTests {
+	
+	@Autowired
+	private ApplicationContext applicationContext;
 
 	@Test
 	void contextLoads() {
+		assertNotNull(applicationContext, "application context should be started");
 	}
 
 }
